a9ebb47261a74e79a95002750764c4f9a5d01018
Taha Hashmat <taha.hashmat@ontariotechu.net>
2021-11-30 13:07:40 -0500

    Nav Bar Implemented

--- Mobile_Project/app/src/main/java/com/example/mobile_project/MainActivity2.java
+++ Mobile_Project/app/src/main/java/com/example/mobile_project/MainActivity2.java
@@ -1,8 +1,14 @@
 package com.example.mobile_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +16,30 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.nav_map:
+                    selectedFragment = new MapFragment();
+                    break;
+                case R.id.nav_user:
+                    selectedFragment = new UserFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+            return true;
+
+        });
+
     }
+
 }
